66b6646ac15b1b9d690b5d5263d3b37c6eb70294
unknown <cataniafrancodev@gmail.com>
2023-08-07 13:58:06 -0300

    Firebase configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './pages/auth/auth.component';
@@ -14,7 +20,6 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { AuthFormsComponent } from './components/auth-forms/auth-forms.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -36,6 +41,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'unicomer-3b7d2',
+    appId: '1:340193881749:web:d5f8472a61e99e76baf59c',
+    storageBucket: 'unicomer-3b7d2.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCFayNOYiPhXrC3jPk8AIuvy_Vnv77A9ag',
+    authDomain: 'unicomer-3b7d2.firebaseapp.com',
+    messagingSenderId: '340193881749',
+  },
+};
