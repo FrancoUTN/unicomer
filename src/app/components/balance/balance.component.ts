9a96da19bd5780129a22d791d60d5fd92935a0c8
unknown <cataniafrancodev@gmail.com>
2023-08-17 17:53:20 -0300

    getCurrentUserBalanceHistory

--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -3,6 +3,7 @@ import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
 import DataLabelsPlugin from 'chartjs-plugin-datalabels';
+import { TransactionService } from 'src/app/services/transaction.service';
 
 @Component({
   selector: 'app-balance',
@@ -11,7 +12,6 @@ import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 })
 export class BalanceComponent {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
@@ -33,11 +33,22 @@ export class BalanceComponent {
   };
   public barChartType: ChartType = 'bar';
   public barChartPlugins = [DataLabelsPlugin];
-
   public barChartData: ChartData<'bar'> = {
     labels: ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'],
     datasets: [
       { data: [65000, 59000, 80000, 81000, 56000, 55, 40000], label: 'Esta semana' },
     ],
   };
+
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit() {
+    this.transactionService.getCurrentUserBalanceHistory().then(
+      days => {
+        console.log(days);
+        console.log(this.barChartData.datasets[0].data);
+        // this.barChartData.datasets[0].data = days;
+      }
+    )
+  }
 }
--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,6 +1,12 @@
 import { Injectable, inject } from '@angular/core';
 import { Firestore, collection, getDocs, or, orderBy, query, where } from '@angular/fire/firestore';
 import { AuthService } from './auth.service';
+import * as dayjs from 'dayjs';
+
+interface Day {
+  date: Date,
+  dayBalance: number
+}
 
 @Injectable({
   providedIn: 'root'
@@ -28,37 +34,90 @@ export class TransactionService {
     )
   }
 
-  getCurrentUserBalance() {
+  async getCurrentUserBalance() {
     const q = this.queryCurrentUserTransactions()
-		return getDocs(q).then(qsTransactions => {
-			let accuBalance: number = 0
-			qsTransactions.forEach(qdsTransaction => {
-        if (!this.currentUser) {
-          throw new Error('There\'s no current user to calculate balance')
+		const qsTransactions = await getDocs(q);
+    let accuBalance: number = 0;
+    qsTransactions.forEach(qdsTransaction => {
+      if (!this.currentUser) {
+        throw new Error('There\'s no current user to calculate balance');
+      }
+      const transaction = qdsTransaction.data();
+      if (transaction.type === 'transfer') {
+        if (transaction.sender === this.currentUser.uid) {
+          accuBalance -= transaction.amount;
         }
-				const transaction = qdsTransaction.data()
-				if (transaction.type === 'transfer') {
-					if (transaction.sender === this.currentUser.uid) {
-						accuBalance -= transaction.amount
-					}
-					else if (transaction.receiver === this.currentUser.uid) {
-						accuBalance += transaction.amount
-					}
-					else {
-						throw new Error('Transfer not related to the user')
-					}
-				}
-				else if (transaction.type === 'deposit' || transaction.type === 'loan') {
-					accuBalance += transaction.amount
-				}
-				else if (transaction.type === 'payment' || transaction.type === 'withdrawal') {
-					accuBalance -= transaction.amount
-				}
-				else {
-					throw new Error('Transaction with unknown type')
-				}
-			})
-			return accuBalance
-    })
-  }  
+        else if (transaction.receiver === this.currentUser.uid) {
+          accuBalance += transaction.amount;
+        }
+        else {
+          throw new Error('Transfer not related to the user');
+        }
+      }
+      else if (transaction.type === 'deposit' || transaction.type === 'loan') {
+        accuBalance += transaction.amount;
+      }
+      else if (transaction.type === 'payment' || transaction.type === 'withdrawal') {
+        accuBalance -= transaction.amount;
+      }
+      else {
+        throw new Error('Transaction with unknown type');
+      }
+    });
+    return accuBalance;
+  }
+
+  async getCurrentUserBalanceHistory() {
+    const q = this.queryCurrentUserTransactions();
+		const qsTransactions = await getDocs(q);
+    let accuTotalBalance: number = 0;
+    // const days: Day[] = [];
+    const days: number[] = [];
+    let daysIndex = 0;
+    let actualDay: any;
+    qsTransactions.forEach(qdsTransaction => {
+      if (!this.currentUser) {
+        throw new Error('There\'s no current user to calculate balance history');
+      }
+      const transaction = qdsTransaction.data();
+      const transactionDate = dayjs(transaction.date.toDate());
+      const transactionAmount = Number(transaction.amount);
+      const transactionType = transaction.type;
+
+      // One-time initialization
+      if (!actualDay) {
+        actualDay = transactionDate;
+      }
+
+      while (!actualDay.isSame(transactionDate, 'day')) {
+        days[daysIndex] = accuTotalBalance;
+        daysIndex++;
+        actualDay = actualDay.add(1, 'day');
+      }
+
+      if (transactionType === 'transfer') {
+        if (transaction.sender === this.currentUser.uid) {
+          accuTotalBalance -= transactionAmount;
+        }
+        else if (transaction.receiver === this.currentUser.uid) {
+          accuTotalBalance += transactionAmount;
+        }
+        else {
+          throw new Error('Transfer not related to the user');
+        }
+      }
+      else if (transactionType === 'deposit' || transactionType === 'loan') {
+        accuTotalBalance += transactionAmount;
+      }
+      else if (transactionType === 'payment' || transactionType === 'withdrawal') {
+        accuTotalBalance -= transactionAmount;
+      }
+      else {
+        throw new Error('Transaction with unknown type');
+      }
+    });
+    days[daysIndex] = accuTotalBalance;
+    return days;
+  }
+
 }
