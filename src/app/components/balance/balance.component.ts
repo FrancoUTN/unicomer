9d9fafc0443988f6d221a1a63ec06940a3a80ce7
unknown <cataniafrancodev@gmail.com>
2023-08-17 12:34:11 -0300

    testing balance

--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -35,7 +35,7 @@ export class BalanceComponent {
   public barChartPlugins = [DataLabelsPlugin];
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'],
     datasets: [
       { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
       { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
