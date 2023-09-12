0a66857d7cfc2f619009a3c14adcb61e9044d7e2
unknown <cataniafrancodev@gmail.com>
2023-09-12 10:39:18 -0300

    transfer amount validations

--- src/app/pages/transfer/transfer.component.ts
+++ src/app/pages/transfer/transfer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { TransactionService } from 'src/app/services/transaction.service';
 
 import { UserService } from 'src/app/services/user.service';
 
@@ -8,11 +10,51 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent {
+	isLoading: boolean = true;
   otherUsers: Array<any> = [];
+  selectedUser: any;
+  transferAmount = new FormControl('');
+  notEnoughBalanceError: boolean = false;
+  transferAmountNotDefinedError: boolean = true;
+  invalidTransferAmountError: boolean = false;
+	balance: number|any;
+	strBalance: string = '...';
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private transactionService: TransactionService,
+    private userService: UserService) { }
 
   async ngOnInit() {
     this.otherUsers = await this.userService.getEveryOtherUser();
+    const balance = await this.transactionService.getCurrentUserBalance();
+    this.setBalanceProperties(balance);
+    // Validation
+    this.transferAmount.valueChanges.subscribe(value => {
+      this.transferAmountNotDefinedError = false;
+      this.notEnoughBalanceError = false;
+      this.invalidTransferAmountError = false;
+      if (!value) {
+        this.transferAmountNotDefinedError = true;
+      }
+      else {
+        const amount = Number(value);
+        if (amount > this.balance) {
+          this.notEnoughBalanceError = true;
+        }
+        if (amount <= 0) {
+          this.invalidTransferAmountError = true;
+        }
+      }
+    });
+  }
+
+  setBalanceProperties(balance: number) {
+    this.balance = balance;
+    const strBalance = balance.toString();
+    this.strBalance = strBalance.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+
+  onUserClick(user: any) {
+    this.selectedUser = user;
   }
 }
