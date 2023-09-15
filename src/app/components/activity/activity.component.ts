f7341c97ad45c5866d0a2aea80afdeebeeb3f671
unknown <cataniafrancodev@gmail.com>
2023-09-15 16:53:02 -0300

    activity pages

--- src/app/components/activity/activity.component.ts
+++ src/app/components/activity/activity.component.ts
@@ -9,24 +9,84 @@ import { TransactionService } from 'src/app/services/transaction.service';
   styleUrls: ['./activity.component.css']
 })
 export class ActivityComponent {
-  transactions: Array<any> = [];
+  allTransactions: Array<any> = [];
+  pages: Array<Array<any>> = [];
+  currentPageIndex: number = 0;
+  showingItems: string = '';
+  totalItems: string = '';
   isMobile: boolean = false;
   currentUrl: string = '';
-	isLoading: boolean = true;
+	isLoading: boolean = false;
 
   constructor(
     private transactionService: TransactionService,
     private router: Router) {}
 
-  ngOnInit() {
-    this.transactionService.loadCurrentUserTransactionsArray(this.transactions)
-      .then(() => this.isLoading = false);
-
+  ngOnInit() {    
     const innerWidth = window.innerWidth;
     this.isMobile = innerWidth <= 900;
     
     this.router.events.subscribe(() => {
       this.currentUrl = this.router.url;
+      if (this.currentUrl === '/transactions') {
+        this.loadAllTransactions();
+      } else {
+        this.loadTransactionPages();
+      }
     });
+  }
+
+  async loadAllTransactions() {
+    this.isLoading = true;
+    try {
+      await this.transactionService.loadCurrentUserTransactionsArray(this.allTransactions);
+    }
+    catch (e) {
+      console.log(e);
+    }
+    this.isLoading = false;
+  }
+
+  async loadTransactionPages() {
+    this.isLoading = true;
+    const transactions: Array<any> = [];
+    try {
+      await this.transactionService.loadCurrentUserTransactionsArray(transactions);
+      this.totalItems = transactions.length.toString();
+      for (let i = 0, k = 0; k < transactions.length; i++) {
+        this.pages[i] = [];
+        for (let j = 0; j < 5; j++, k++) {
+          if (!transactions[k]) {
+            break;
+          }
+          this.pages[i][j] = transactions[k];
+        }
+      }
+      this.setShowing();
+    }
+    catch (e) {
+      console.log(e);
+    }
+    this.isLoading = false;
+  }
+  
+  onPreviousPageClick() {
+    if (this.currentPageIndex > 0) {
+     this.currentPageIndex--;
+    }
+    this.setShowing();
+  }
+
+  onForwardPageClick() {
+    if (this.currentPageIndex < this.pages.length - 1) {
+      this.currentPageIndex++;
+    }
+    this.setShowing();
+  }
+
+  setShowing() {
+    const from = (this.currentPageIndex * 5) + 1;
+    const to = from + this.pages[this.currentPageIndex].length - 1;
+    this.showingItems = `${from}-${to}`;
   }
 }
